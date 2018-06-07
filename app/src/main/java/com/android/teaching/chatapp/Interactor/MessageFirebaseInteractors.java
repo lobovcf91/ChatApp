44580b175edbc5049a505aa8085d83c27ec9417c
lobo <lobo.soldado@gmail.com>
2018-06-07 15:17:51 +0200

    examen

---
+++ app/src/main/java/com/android/teaching/chatapp/Interactor/MessageFirebaseInteractors.java
@@ -0,0 +1,60 @@
+package com.android.teaching.chatapp.Interactor;
+
+import android.provider.ContactsContract;
+import android.util.Log;
+
+import com.android.teaching.chatapp.model.MessageModel;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class MessageFirebaseInteractors {
+    private ArrayList<MessageModel> messages = new ArrayList<>();
+
+public void getMessages (final MessageInteractorCallback callback){
+
+    //1-llamar a firebasa
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference myReference = firebaseDatabase.getReference("messages");
+    myReference.addListenerForSingleValueEvent(new ValueEventListener() {
+
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot){
+            for (DataSnapshot nodojuego: dataSnapshot.getChildren()) {
+                MessageModel model = nodojuego.getValue(MessageModel.class);
+                Log.d("Firebase Interactor", "Message: "+ model.getText());
+                messages.add(model);
+            }
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+
+        }
+    });
+
+}
+
+    public  ArrayList<MessageModel> getMessages() {
+        return messages;
+    }
+
+    public MessageModel getGameWithId(int id) {
+        //Obtener de games el juego con el identificador id
+        for (MessageModel message : messages) {
+            if (message.getId() == id) {
+                return message;
+            }
+        }
+        return null;
+
+
+
+}
---
+++ app/src/main/java/com/android/teaching/chatapp/Interactor/MessageInteractorCallback.java
@@ -0,0 +1,5 @@
+package com.android.teaching.chatapp.Interactor;
+
+public interface MessageInteractorCallback {
+    void OnGamesAviable();
+}
