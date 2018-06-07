e975d4363e2d79b7eda704976cd0e8fc14b0ae31
lobo <lobo.soldado@gmail.com>
2018-06-07 12:03:05 +0200

    examen

---
+++ app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
@@ -0,0 +1,33 @@
+package com.android.teaching.chatapp;
+
+import android.annotation.SuppressLint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+
+    @SuppressLint("ResourceType")
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+        }
+
+        return true;
+    }
+}
